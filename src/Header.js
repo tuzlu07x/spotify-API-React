24ed3075f88d62403ae617ef2d2af08ab7a7696b
tuzlu07x <fatihtuzlu07@gmail.com>
2023-01-07 17:33:17 +0300

    react project added.

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '4px', transform: 'scale(0.8)' }}
+  >
+  </Box>
+);
+
+export default function BasicCard() {
+  return (
+    <Card sx={{ boxShadow:5,background:'linear-gradient(45deg, #006400 30%, #7FFFD4 90%)',color:'white' }}>
+      <CardContent>
+        <Typography align='center' variant="h4" component="div">
+          Spotify API
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
